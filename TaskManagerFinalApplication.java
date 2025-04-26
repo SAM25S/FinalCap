3e47a63ad105734244784ecb86e4aa0a51b96e0d
SAM25S <samruddhipawar218@gmail.com>
2025-04-27 00:37:41 +0530

    Add files via upload

---
+++ TaskManagerFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.TaskManagerFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerFinalApplication.class, args);
+	}
+
+}
